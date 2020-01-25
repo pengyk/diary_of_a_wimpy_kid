7a1fbef6301f3b8405957b982f7b47910d03aad2
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-01-25 15:44:26 -0500

    Add logo (again)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import {Modal,
-  Button, Container, Divider, Grid, Header, Icon, Image, List, Menu, Responsive, Segment, Sidebar, Visibility,
-  Form, TextArea
-} from 'semantic-ui-react'
-import "./App.css"
-import firebase from "./firebase"
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import {
+  Modal,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+  Form,
+  TextArea
+} from "semantic-ui-react";
+import "./App.css";
+import firebase from "./firebase";
 
 // size may also be a plain string using the presets 'large' or 'compact'
 const size = {
-  width: '100%',
-  height: 300,
+  width: "100%",
+  height: 300
 };
-const view = 'list'; // or 'coverart'
-const theme = 'black'; // or 'white'
+const view = "list"; // or 'coverart'
+const theme = "black"; // or 'white'
 // Heads up!
 // We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
 // For more advanced usage please check Responsive docs under the "Usage" section.
 const getWidth = () => {
-  const isSSR = typeof window === 'undefined'
+  const isSSR = typeof window === "undefined";
 
-  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
-}
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
+};
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
@@ -29,51 +43,57 @@ const getWidth = () => {
  */
 const HomepageHeading = ({ mobile }) => (
   <Container text>
+    <div
+      className="logo-container"
+      style={{
+        marginTop: mobile ? "1.5em" : "3em"
+      }}
+    >
+      <Image src="/logo-flat.png" size="medium" />
+      <Header
+        as="h1"
+        content="MindBook"
+        inverted
+        style={{
+          fontSize: mobile ? "2em" : "4em",
+          fontWeight: "normal"
+        }}
+      />
+    </div>
     <Header
-      as='h1'
-      content='Write 2 Heal'
+      as="h2"
+      content="Let your words speak for you."
       inverted
       style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em"
       }}
     />
-    <Header
-      as='h2'
-      content='Let your words speak for you.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge' a href="#newEntry">
+    <Button primary size="huge" a href="#newEntry">
       Enter today's diary entry
-      <Icon name='right arrow' />
+      <Icon name="right arrow" />
     </Button>
   </Container>
-)
+);
 
 HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
+  mobile: PropTypes.bool
+};
 
 /* Heads up!
  * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
  * It can be more complicated, but you can create really flexible markup.
  */
 class DesktopContainer extends Component {
-  state = {}
+  state = {};
 
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
 
   render() {
-    const { children } = this.props
-    const { fixed } = this.state
+    const { children } = this.props;
+    const { fixed } = this.state;
 
     return (
       <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
@@ -84,21 +104,23 @@ class DesktopContainer extends Component {
         >
           <Segment
             inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
+            textAlign="center"
+            style={{ minHeight: 700, padding: "1em 0em" }}
             vertical
           >
             <Menu
-              fixed={fixed ? 'top' : null}
+              fixed={fixed ? "top" : null}
               inverted={!fixed}
               pointing={!fixed}
               secondary={!fixed}
-              size='large'
+              size="large"
             >
               <Container>
-                <Menu.Item position='right'>
-                  <Menu.Item a href="#newEntry">New Entry</Menu.Item>
-                  <Menu.Item as='a'>Logs</Menu.Item>
+                <Menu.Item position="right">
+                  <Menu.Item a href="#newEntry">
+                    New Entry
+                  </Menu.Item>
+                  <Menu.Item as="a">Logs</Menu.Item>
                 </Menu.Item>
               </Container>
             </Menu>
@@ -108,24 +130,24 @@ class DesktopContainer extends Component {
 
         {children}
       </Responsive>
-    )
+    );
   }
 }
 
 DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
+  children: PropTypes.node
+};
 
 class MobileContainer extends Component {
-  state = {}
+  state = {};
 
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
 
-  handleToggle = () => this.setState({ sidebarOpened: true })
+  handleToggle = () => this.setState({ sidebarOpened: true });
 
   render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
 
     return (
       <Responsive
@@ -135,37 +157,35 @@ class MobileContainer extends Component {
       >
         <Sidebar
           as={Menu}
-          animation='push'
+          animation="push"
           inverted
           onHide={this.handleSidebarHide}
           vertical
           visible={sidebarOpened}
         >
-          <Menu.Item as='a' active>
+          <Menu.Item as="a" active>
             Home
           </Menu.Item>
-          <Menu.Item as='a'>Work</Menu.Item>
-          <Menu.Item as='a'>Company</Menu.Item>
-          <Menu.Item as='a'>Careers</Menu.Item>
-          <Menu.Item as='a'>New Entry</Menu.Item>
-          <Menu.Item as='a'>Sign Up</Menu.Item>
+          <Menu.Item as="a">Work</Menu.Item>
+          <Menu.Item as="a">Company</Menu.Item>
+          <Menu.Item as="a">Careers</Menu.Item>
+          <Menu.Item as="a">New Entry</Menu.Item>
+          <Menu.Item as="a">Sign Up</Menu.Item>
         </Sidebar>
 
         <Sidebar.Pusher dimmed={sidebarOpened}>
           <Segment
             inverted
-            textAlign='center'
-            style={{ minHeight: 350, padding: '1em 0em' }}
+            textAlign="center"
+            style={{ minHeight: 350, padding: "1em 0em" }}
             vertical
           >
             <Container>
-              <Menu inverted pointing secondary size='large'>
-                <Menu.Item position='right' a href="#newEntry">
+              <Menu inverted pointing secondary size="large">
+                <Menu.Item position="right" a href="#newEntry">
                   New Entry
                 </Menu.Item>
-                <Menu.Item>
-                  Logs
-                </Menu.Item>
+                <Menu.Item>Logs</Menu.Item>
               </Menu>
             </Container>
             <HomepageHeading mobile />
@@ -174,55 +194,59 @@ class MobileContainer extends Component {
           {children}
         </Sidebar.Pusher>
       </Responsive>
-    )
+    );
   }
 }
 
 MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
+  children: PropTypes.node
+};
 
 const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
     <MobileContainer>{children}</MobileContainer>
   </div>
-)
+);
 
 ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
+  children: PropTypes.node
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       time: new Date().toLocaleString(),
-      text: '',
-      time: '',
-      items: [],
-    }
+      text: "",
+      time: "",
+      items: []
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-
-
   componentDidMount() {
     var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date + ' ' + time;
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + " " + time;
     this.setState({ time: dateTime });
     console.log("var" + dateTime);
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
+    const itemsRef = firebase.database().ref("items");
+    itemsRef.on("value", snapshot => {
       let items = snapshot.val();
       let newState = [];
       for (let item in items) {
         newState.push({
           id: item,
           time: items[item].time,
-          text: items[item].text,
+          text: items[item].text
         });
       }
       this.setState({
@@ -231,34 +255,38 @@ class App extends Component {
     });
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({ text: event.target.value });
-  }
+  };
 
-  handleTime = (event) => {
+  handleTime = event => {
     var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date + ' ' + time;
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + " " + time;
     this.setState({ time: dateTime });
     console.log("var" + dateTime);
-  }
+  };
 
-
-
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
+    const itemsRef = firebase.database().ref("items");
     const item = {
       time: this.state.time,
-      text: this.state.text,
-    }
+      text: this.state.text
+    };
     itemsRef.push(item);
     this.setState({
       text: "",
-      time: new Date().toLocaleString(),
+      time: new Date().toLocaleString()
     });
-  }
+  };
 
   removeItem(itemId) {
     const itemRef = firebase.database().ref(`/items/${itemId}`);
@@ -268,10 +296,13 @@ class App extends Component {
   render() {
     return (
       <ResponsiveContainer>
-        <Segment style={{ padding: '8em 0em' }} vertical id="newEntry">
+        <Segment style={{ padding: "8em 0em" }} vertical id="newEntry">
           <div className="paddingText">
             <Form>
-              <TextArea rows={16} placeholder='Spill your heart...' style={{ minHeight: 200 }}
+              <TextArea
+                rows={16}
+                placeholder="Spill your heart..."
+                style={{ minHeight: 200 }}
                 value={this.state.text}
                 onChange={this.handleChange}
               />
@@ -283,52 +314,55 @@ class App extends Component {
         view={view}
         theme={theme}
       /> */}
-          <Grid container stackable verticalAlign='middle'>
+          <Grid container stackable verticalAlign="middle">
+            <Grid.Row></Grid.Row>
             <Grid.Row>
-
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column textAlign='center'>
-                <Button size='huge'
-                  onClick={this.handleSubmit}
-
-                >Submit daily entry</Button>
+              <Grid.Column textAlign="center">
+                <Button size="huge" onClick={this.handleSubmit}>
+                  Submit daily entry
+                </Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Segment>
 
-        <Segment style={{ padding: '0em' }} vertical>
-          <Grid celled='internally' columns='equal' stackable>
-            <Grid.Row textAlign='center'>
-              <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+        <Segment style={{ padding: "0em" }} vertical>
+          <Grid celled="internally" columns="equal" stackable>
+            <Grid.Row textAlign="center">
+              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
                 <List>
-                  {this.state.items.map((item) => {
+                  {this.state.items.map(item => {
                     return (
                       <List.Item key={item.id}>
                         <h3>{item.time}</h3>
                         <p>Mood found: {item.text}</p>
                         <Modal trigger={<Button>Show details</Button>}>
-                          <Modal.Header>Entry date and time: {item.time}</Modal.Header>
+                          <Modal.Header>
+                            Entry date and time: {item.time}
+                          </Modal.Header>
                           <Modal.Content image scrolling>
-                            <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png' wrapped />
+                            <Image
+                              size="medium"
+                              src="https://react.semantic-ui.com/images/wireframe/image.png"
+                              wrapped
+                            />
 
                             <Modal.Description>
                               <Header>Details</Header>
-                              <p>
-                                {item.text}
-                              </p>
+                              <p>{item.text}</p>
                             </Modal.Description>
                           </Modal.Content>
                           <Modal.Actions>
                             <Button primary>
-                              Proceed <Icon name='chevron right' />
+                              Proceed <Icon name="chevron right" />
                             </Button>
                           </Modal.Actions>
                         </Modal>
-                        <Button onClick={() => this.removeItem(item.id)}>Remove Item</Button>
+                        <Button onClick={() => this.removeItem(item.id)}>
+                          Remove Item
+                        </Button>
                       </List.Item>
-                    )
+                    );
                   })}
                 </List>
               </Grid.Column>
@@ -336,54 +370,56 @@ class App extends Component {
           </Grid>
         </Segment>
 
-        <Segment style={{ padding: '8em 0em' }} vertical>
+        <Segment style={{ padding: "8em 0em" }} vertical>
           <Container text>
-            <Header as='h3' style={{ fontSize: '2em' }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               Breaking The Grid, Grabs Your Attention
-        </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Instead of focusing on content creation and hard work, we have learned how to master the
-              art of doing nothing by providing massive amounts of whitespace and generic content that
-              can seem massive, monolithic and worth your attention.
-        </p>
-            <Button as='a' size='large'>
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Instead of focusing on content creation and hard work, we have
+              learned how to master the art of doing nothing by providing
+              massive amounts of whitespace and generic content that can seem
+              massive, monolithic and worth your attention.
+            </p>
+            <Button as="a" size="large">
               Read More
-        </Button>
+            </Button>
 
             <Divider
-              as='h4'
-              className='header'
+              as="h4"
+              className="header"
               horizontal
-              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+              style={{ margin: "3em 0em", textTransform: "uppercase" }}
             >
-              <a href='#'>Case Studies</a>
+              <a href="#">Case Studies</a>
             </Divider>
 
-            <Header as='h3' style={{ fontSize: '2em' }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               Did We Tell You About Our Bananas?
-        </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-              it's really true. It took years of gene splicing and combinatory DNA research, but our
-              bananas can really dance.
-        </p>
-            <Button as='a' size='large'>
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Yes I know you probably disregarded the earlier boasts as
+              non-sequitur filler content, but it's really true. It took years
+              of gene splicing and combinatory DNA research, but our bananas can
+              really dance.
+            </p>
+            <Button as="a" size="large">
               I'm Still Quite Interested
-        </Button>
+            </Button>
           </Container>
         </Segment>
 
-        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+        <Segment inverted vertical style={{ padding: "5em 0em" }}>
           <Container>
             <Grid divided inverted stackable>
               <Grid.Row>
                 <Grid.Column width={3}>
-                  <Header inverted as='h4' content='Members' />
+                  <Header inverted as="h4" content="Members" />
                   <List link inverted>
-                    <List.Item as='a'>Hong Kun, Tian</List.Item>
-                    <List.Item as='a'>Jia Rong, Shao</List.Item>
-                    <List.Item as='a'>Yun Kai, Peng</List.Item>
-                    <List.Item as='a'>Made w/ ❤️at ConUHacks V</List.Item>
+                    <List.Item as="a">Hong Kun, Tian</List.Item>
+                    <List.Item as="a">Jia Rong, Shao</List.Item>
+                    <List.Item as="a">Yun Kai, Peng</List.Item>
+                    <List.Item as="a">Made w/ ❤️at ConUHacks V</List.Item>
                   </List>
                 </Grid.Column>
               </Grid.Row>
@@ -391,8 +427,8 @@ class App extends Component {
           </Container>
         </Segment>
       </ResponsiveContainer>
-    )
+    );
   }
 }
 
-export default App
+export default App;
